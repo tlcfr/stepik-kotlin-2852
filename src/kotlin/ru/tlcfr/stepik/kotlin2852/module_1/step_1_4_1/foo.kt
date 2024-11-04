f40a496c06d855c069180e2a3b00adf661156552
tlcfr <tatarinov.lcfr@gmail.com>
2024-11-05 01:15:06 +0300

    step_1_4_1

---
+++ src/kotlin/ru/tlcfr/stepik/kotlin2852/module_1/step_1_4_1/foo.kt
@@ -0,0 +1,11 @@
+package ru.tlcfr.stepik.kotlin2852.module_1.step_1_4_1
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+    (if (toUpperCase) name.toUpperCase() else name) + number
+
+fun useFoo() = listOf(
+    foo("a"),
+    foo("b", number = 1),
+    foo("c", toUpperCase = true),
+    foo(name = "d", number = 2, toUpperCase = true)
+)
