63ee9c7b36f2c5a2101d4f9d1a1e4f84bd16dcfe
tlcfr <tatarinov.lcfr@gmail.com>
2024-11-07 00:41:52 +0300

    1.10_extension-functions

---
+++ src/kotlin/ru/tlcfr/stepik/kotlin2852/module_1/1.10_extension-functions.kt
@@ -0,0 +1,6 @@
+package ru.tlcfr.stepik.kotlin2852.module_1
+
+data class RationalNumber(val numerator: Int, val denominator: Int)
+
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
